c5a3705f8a8d31c085027e4baeaea67a24e07b51
tbrindley <travisbrindley@gmail.com>
2017-09-03 23:01:01 -0400

    replaced mySQL database & getters / setters for hibernate

--- src/main/java/com/userregistration/factory/UserFactory.java
+++ src/main/java/com/userregistration/factory/UserFactory.java
@@ -8,6 +8,8 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.criterion.Order;
 
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 /**
@@ -25,11 +27,14 @@ public class UserFactory {
         users.addOrder(Order.desc("dateEntered"));
         return (ArrayList<Users>) users.list();
     }
+
     public static void addUser(String firstName, String lastName, String address1, String address2, String city, String state, String zip){
         Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
         SessionFactory sessionFact = cfg.buildSessionFactory();
         Session session = sessionFact.openSession();
-        Transaction tx = session.beginTransaction();
+        Transaction transaction = session.beginTransaction();
+
+        Timestamp submitTime = new Timestamp(System.currentTimeMillis());
 
         Users newUser = new Users();
         newUser.setFirstName(firstName);
@@ -40,9 +45,11 @@ public class UserFactory {
         newUser.setState(state);
         newUser.setZip(zip);
         newUser.setCountry("US");
+        newUser.setDateEntered(submitTime);
 
+        System.out.println(newUser.getFirstName());
         session.save(newUser);
-        tx.commit();
+        transaction.commit();
         session.close();
     }
 }
